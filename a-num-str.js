1f3942a9e3b10ad820b6962836fb2dadb9360b63
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-03-24 12:16:45 +0500

    Add files via upload

---
+++ a-num-str.js
@@ -0,0 +1,7 @@
+function aNumStr(a, b) {
+    if (a === void 0) { a = 5; }
+    if (b === void 0) { b = '5'; }
+    var sUMab = (a + parseInt(b));
+    return sUMab;
+}
+console.log(aNumStr());
---
+++ a-num-str.ts
@@ -0,0 +1,7 @@
+function aNumStr( a: number=5, b: string='5'){
+
+ const sUMab: number = (a + parseInt(b));
+ return sUMab;
+
+}
+console.log(aNumStr());
