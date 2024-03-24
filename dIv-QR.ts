57f8f80503d7095c94428caf3d5e711e88a94aea
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-03-24 12:15:18 +0500

    Add files via upload

---
+++ dIv-QR.ts
@@ -0,0 +1,10 @@
+function QR(a: number= 22, b: number=10) : any{
+
+    const findoutQR: any= {
+        a: 22,
+        b: 10,
+}
+return console.log("Qoutient is:", Math.floor(a/b), "Reminder is:", a % b);
+}
+
+QR();
---
+++ div-QR.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function QR(a = 22, b = 10) {
+    const findoutQR = {
+        a: 22,
+        b: 10,
+    };
+    return console.log("Qoutient is:", Math.floor(a / b), "Reminder is:", a % b);
+}
+QR();
