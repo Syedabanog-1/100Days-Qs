313a1d64c26411112280f836da4152a1bf4cbd4a
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-04-13 03:43:05 +0500

    Add files via upload

---
+++ Quiz88.js
@@ -0,0 +1,11 @@
+"use strict";
+// *Question 88:* Rounding to the Nearest Integer: Write a function that takes a decimal number as input and returns the number rounded to the nearest integer.
+Object.defineProperty(exports, "__esModule", { value: true });
+// This function rounds a decimal number to the nearest whole number
+function roundToNearestInteger(num) {
+    return Math.round(num); // Rounds the number
+}
+// Example: Rounding a decimal
+console.log(roundToNearestInteger(4.7)); // Outputs: 5
+console.log(roundToNearestInteger(4.4)); // Outputs: 4
+// This shows how the function rounds numbers either up or down.
--- multiliteral.js
+++ multiliteral.js
@@ -1,6 +1,7 @@
-var sent_a = 'I like tourism'; //sent_a (sentence)
-var sent_b = 'Tourism is  so good i love it a lot because i am interested in  exploring things and places';
-var sent_c = 'Tourism makes fun and enjoyment';
-var sent_d = 'By tourism we can meet people belong to different culture';
-var sent_e = 'by tourism we got sharpness improved.';
-console.log("\n\t ".concat(sent_a, ", \n\t").concat(sent_b, ", \n\t").concat(sent_c, ", \n\t").concat(sent_d, ", \n\t").concat(sent_e));
+"use strict";
+let sent_a = 'I like tourism'; //sent_a (sentence)
+let sent_b = 'Tourism is  so good i love it a lot because i am interested in  exploring things and places';
+let sent_c = 'Tourism makes fun and enjoyment';
+let sent_d = 'By tourism we can meet people belong to different culture';
+let sent_e = 'by tourism we got sharpness improved.';
+console.log(`\n\t ${sent_a}, \n\t${sent_b}, \n\t${sent_c}, \n\t${sent_d}, \n\t${sent_e}`);
