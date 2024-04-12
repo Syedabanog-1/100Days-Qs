261c79893a1e20043c19579b7daa840e25caf69b
Syedabanog-1 <160377198+Syedabanog-1@users.noreply.github.com>
2024-04-12 12:18:16 +0500

    Add files via upload

---
+++ Quiz102.js
@@ -0,0 +1,7 @@
+"use strict";
+// *Question 102:* Calculate and log the absolute difference between the number -5 and 5.
+Object.defineProperty(exports, "__esModule", { value: true });
+// Calculates the absolute difference between -5 and 5
+const difference = Math.abs(-5 - (5));
+console.log(difference); // Outputs: 10
+// This line shows the absolute value of the difference between -5 and 5, ignoring the sign.
